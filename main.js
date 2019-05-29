6a9db8e9ea985174341a7f1c791a0c8b4e182637
Sherman Liu <shermanliu03@gmail.com>
2019-05-30 00:11:26 +0800

    added main calculator functions

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // TODO: add display variable
-let inputNum = ""
+let inputDisplay = ""
 let display = document.querySelector('.display > p')
 let currentOperator = "";
-
+let numX = 0;
+let numY = 0;
 function add (numX, numY) {
     return numX + numY;
 }
@@ -12,15 +13,36 @@ function subtract (numX, numY) {
 }
 
 function multiply (numX, numY) {
+    return numX * numY;
 
 }
 
 function divide (numX, numY) {
-
+    if (numY == 0) {
+        alert("Don't be a smart ass");
+        return 0
+    }
+    return numX / numY;
 }
 
-function operator (opeartion, displayNum, inputNum) {
+function operator (currentOperator, numX, numY) {
+    switch (currentOperator) {
 
+        case '+':
+
+            return add(numX, numY)
+            break;
+        case '-':
+            return subtract(numX, numY)
+            break;
+        case '÷':
+            return divide(numX, numY)
+            break;
+        case 'x':
+
+            return multiply(numX, numY)
+            break;
+    }
 }
 // TODO: Calculator operation
 /*
@@ -35,22 +57,94 @@ operation on the 'inputNum' and the 'displayNum.''
 */
 
 let calculatorBtns = document.querySelectorAll('.button');
+let equalsBtn = document.querySelector('.equals');
 calculatorBtns = [...calculatorBtns]
-
+let inputNum = 0;
+let calculating = false;
+let result = false;
 calculatorBtns.forEach((button) => {
     let classes = button.className.split(" ")
     button.addEventListener('click', () => {
-        let value = button.textContent
-        
+        let operatorDisplay = document.querySelector('.operator')
+
+
         if (classes.includes('operation-btn')){
-            if (value == 'x'){
-                inputNum += '*';
+
+            operatorDisplay.textContent = button.textContent;
+            currentOperator = operatorDisplay.textContent
+
+            if (calculating == true) {
+                numY = parseInt(display.textContent)
+                inputDisplay = `${operator(currentOperator, numX, numY)}`;
+                result = true;
+                calculating = false;
+                numX = parseInt(inputDisplay);
             }
-        } else {
-                inputNum += button.textContent
+            else {
+                numX = parseInt(display.textContent)
+                inputDisplay = `0`;
+                calculating = true
+            }
 
         }
+        else if (classes.includes('equals')) {
+               if (currentOperator == ''){
+                   alert("Please choose an operator")
+               } else{
+                   numY = parseInt(display.textContent)
+                   inputDisplay = `${operator(currentOperator, numX, numY)}`;
+                   operatorDisplay.textContent = '';
+                   currentOperator = '';
+                   calculating = false;
+                   result = true;
+               }
 
-        display.textContent = inputNum;
+           }
+           else if (classes.includes('clear')){
+               if (button.textContent == 'AC') {
+                   inputNum = 0;
+                   calculating = false;
+                   numX = 0;
+                   numY = 0;
+                   currentOperator = '';
+                   operatorDisplay.textContent = '';
+                   inputDisplay = 0;
+               }
+               else {
+                   if (operatorDisplay.textContent){
+                       numY = 0;
+                       inputNum = 0;
+                       inputDisplay = 0;
+                   }
+                   else {
+                       numX = 0
+                       inputNum = 0;
+                       inputDisplay = 0;
+                   }
+               }
+           }
+             else {
+                if (parseInt(inputDisplay) == 0){
+                    inputDisplay = button.textContent;
+                }
+                else{
+                    if (result) {
+                        result = false;
+                         inputDisplay = button.textContent;
+                    }
+                    else {
+                        inputDisplay += button.textContent
+                    }
+                }
+        }
+
+        display.textContent = inputDisplay;
+        if (display.textContent.length > 10){
+            display.textContent = display.textContent.substr(0, 13)
+        }
     });
 });
+
+document.addEventListener('keydown', (e) => {
+    console.log(e);
+})
