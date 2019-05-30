c0499e8e944e644448c1e29799e85a8654e36432
Sherman Liu <shermanliu03@gmail.com>
2019-05-30 11:18:12 +0800

    revert calculator after adding main functions

--- main.js
+++ main.js
@@ -145,6 +145,5 @@ calculatorBtns.forEach((button) => {
     });
 });
 
-document.addEventListener('keydown', (e) => {
-    console.log(e);
-})
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const operators = ['+', '-', '*', 'x'];
